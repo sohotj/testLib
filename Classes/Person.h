8d3c9eec3021940600e28c39804e0f2947cb129f
唐杰 <sohotj@163.com>
2018-12-22 22:47:37 +0800

    init

---
+++ Classes/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  test
+//
+//  Created by 兰葳蕤 on 2018/12/22.
+//  Copyright © 2018 sohotj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  test
+//
+//  Created by 兰葳蕤 on 2018/12/22.
+//  Copyright © 2018 sohotj. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
